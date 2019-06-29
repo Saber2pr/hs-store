d2e4463457708ae79c9bc0c8631a78cc5c3da46c
saber2pr <saber2pr@gmail.com>
2019-06-29 15:08:40 +0000

    feat(Main.hs): add createStore

--- Main.hs
+++ Main.hs
@@ -12,30 +12,39 @@ data Action = Action {
   , payloadInt :: Integer
 } deriving (Show)
 
-class Store s where
-  dispatch :: Monad m => s -> (s -> m State) -> m s
-  increase :: Integer -> s -> s
-  update   :: String -> s -> s
-  reducer  :: s -> Action -> s
-
 instance Store State where
-  dispatch s f = f s
-  increase n s = State ((+n).count $ s) $ message s
-  update   m s = State (count s) m
+  createStore l a s  = do
+    l nextState
+    return nextState
+    where
+      nextState = reducer s a
+
   reducer s a  = case name a of
     "add" -> increase (payloadInt a) s
     "set" -> update (payloadStr a) s
     _     -> s
 
-main :: IO ()
-main = do
-  next <- dispatch (reducer initialState action_add) listener
-  res <- dispatch (reducer next action_set) listener
-  return ()
+  increase n s = State ((+n) $ count s) $ message s
+  update   m s = State (count s) m
+
+class Store s where
+  createStore :: Monad m => (s -> m ()) -> Action -> s -> m s
+  reducer  :: s -> Action -> s
+
+  increase :: Integer -> s -> s
+  update   :: String -> s -> s
+
+main :: IO State
+main = dispatch action_add initialState
+   >>= dispatch action_set
+   >>= dispatch action_add
+   >>= dispatch action_add
+   >>= dispatch action_add
   where
     initialState = State 0 ""
-    action_add       = Action "add" "" 2
-    action_set       = Action "set" "hello redux" 0
-    listener  s  = do
-      print s
-      return s
+
+    action_add   = Action "add" ""            2
+    action_set   = Action "set" "hello redux" 0
+
+    listener     = print
+    dispatch     = createStore listener
