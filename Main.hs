1138937cab9ade81e324417e5a9e0b7cecb18b22
saber2pr <saber2pr@gmail.com>
2019-06-29 11:43:58 +0000

    feat(Main.hs): add Action datatype

--- Main.hs
+++ Main.hs
@@ -5,16 +5,25 @@ data State = State {
   count :: Integer
 } deriving (Show)
 
+data Action = Action {
+    name :: String
+  , payload :: Integer
+}
+
 class Store s where
   dispatch :: s -> IO ()
   increase :: Integer -> s -> s
+  reducer :: s -> Action -> s
 
 instance Store State where
-  dispatch = print.count
-  increase n s = State $ (+n).count $ s
+  dispatch    = print.count
+  increase n  = State.(+n).count
+  reducer s a = case name a of
+    "add" -> increase (payload a) s
+    _     -> s
 
 main :: IO ()
-main = dispatch $ (reducer.reducer.reducer) initialState -- 3
+main = dispatch $ (reducer initialState action) -- 2
   where
     initialState = State 0
-    reducer = increase 1
+    action = Action "add" 2
